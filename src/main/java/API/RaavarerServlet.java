45e17f109eab0dfdca8a6ac761b3fe12670bdbd6
Kristoffer <s180500@student.dtu.dk>
2020-06-05 10:47:51 +0200

    tabel er sat

---
+++ src/main/java/API/RaavarerServlet.java
@@ -0,0 +1,14 @@
+package API;
+
+import DB.DBConnector;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("raavare")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class RaavarerServlet {
+
+}
---
+++ src/main/java/Data/DBRaavarer.java
@@ -0,0 +1,17 @@
+package Data;
+
+import API.RaavarerServlet;
+
+import javax.ws.rs.*;
+import java.util.List;
+
+public class DBRaavarer implements IDataHandlerDAO {
+
+/*
+    @GET
+    public List<RaavarerServlet> listAllRaavarer() {
+        return ;
+    }
+
+ */
+}
