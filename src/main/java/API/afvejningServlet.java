724280ca2f4b11ce4c9b0456dc9e8ce1494a9636
Simon Søborg <s173255@student.dtu.dk>
2020-06-05 16:06:23 +0200

    Frontend næsten done

---
+++ src/main/java/API/afvejningServlet.java
@@ -0,0 +1,13 @@
+package API;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("afvejning")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class afvejningServlet {
+
+}
