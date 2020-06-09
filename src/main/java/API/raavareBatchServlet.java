2fc9c20887d1a58293c9a0f6128417e0df3a0db2
KEHansen <s195488@student.dtu.dk>
2020-06-09 11:37:26 +0200

    Tilføjelse af RaavareBatchServlet

--- src/main/java/API/raavareBatchServlet.java
+++ src/main/java/API/raavareBatchServlet.java
@@ -1,6 +1,7 @@
 package API;
 
 import Data.DBRaavareBatch;
+import Data.DTO.raavareBatch;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
@@ -10,7 +11,19 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class raavareBatchServlet {
-    DBRaavareBatch dbrb = new DBRaavareBatch();
+    DBRaavareBatch DBrb = new DBRaavareBatch();
+
+    @GET
+    public List<raavareBatch> GetRaavareBatch() {
+        return DBrb.listAllRaavareBatch();
+    }
+
+    @DELETE
+    @Path("{rbId}")
+    public void deleteRaavareBatch(@PathParam("rbId") int rbId) {
+        DBrb.deleteRaavareBatch(rbId);
+    }
 
 }
+
 
