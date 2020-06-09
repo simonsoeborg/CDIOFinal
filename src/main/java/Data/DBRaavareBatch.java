b1bf2875c4413419987f00b2cb17a13193038ea3
KEHansen <s195488@student.dtu.dk>
2020-06-09 11:09:09 +0200

    Tilføjelse af DBRaavareBatch

--- src/main/java/Data/DBRaavareBatch.java
+++ src/main/java/Data/DBRaavareBatch.java
@@ -1,28 +1,63 @@
 package Data;
 
 import DB.DBConnector;
+import Data.DTO.raavareBatch;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DBRaavareBatch {
-
-    private int rbId;
-    private int raavareId;
-    private double maengde;
-    private Connection SQLConn;
+    DBConnector dbc = new DBConnector();
     private String sqlQuery;
-    private List<DBRaavareBatch> rbList;
+    private Connection SQLConn = dbc.createConnection();
+    private List<raavareBatch> raavarebatch;
 
     private DBConnector MySQLConnector = new DBConnector();
 
-    public DBRaavareBatch(int rbId, int raavareId, double maengde) {
-        this.rbId = rbId;
-        this.raavareId = raavareId;
-        this.maengde = maengde;
+    public void fetchAllRaavareBatch() {
+        raavarebatch = new ArrayList<>();
+        raavarebatch = GetAllRaavareBatch();
     }
 
+    public List<raavareBatch> GetAllRaavareBatch() {
+        ArrayList<raavareBatch> data = new ArrayList<>();
+        SQLConn = MySQLConnector.createConnection();
+        if (SQLConn != null) {
+            try {
+                sqlQuery = "SELECT * FROM Raavarebatch";
+                //prepared statement
+                PreparedStatement pstm = SQLConn.prepareStatement(sqlQuery);
+                ResultSet resultSet = pstm.executeQuery();
+                while (resultSet.next()) {
+                    data.add(new raavareBatch(resultSet.getInt("rbId"), resultSet.getInt("raavareId"), resultSet.getDouble("maengde")));
+                }
+                SQLConn.close();
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+        return data;
+    }
+    public List<raavareBatch> listAllRaavareBatch() {
+        fetchAllRaavareBatch();
+        return raavarebatch; }
 
-
-
+    public void deleteRaavareBatch(int rbId) {
+        try {
+            SQLConn = MySQLConnector.createConnection();
+            if (SQLConn != null) {
+                sqlQuery = "DELETE FROM Raavarebatch WHERE rbId= ?";
+                PreparedStatement pstm = SQLConn.prepareStatement(sqlQuery);
+                pstm.setInt(1, rbId);
+                pstm.executeUpdate();
+                SQLConn.close();
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
 }
