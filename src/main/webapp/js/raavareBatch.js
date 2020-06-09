4173637f131f305a21879682936a165f060311bb
KEHansen <s195488@student.dtu.dk>
2020-06-09 10:32:16 +0200

    Tilføjelse af JS

--- src/main/webapp/js/raavareBatch.js
+++ src/main/webapp/js/raavareBatch.js
@@ -1,5 +1,31 @@
+var hostURL = '/CDIOFinal_war_exploded/test/raavarebatch/';
 
+function loadRaavareBatchList() {
+    console.log("Loading Råvare Batches");
+    $.get(hostURL, function (data, textStatus, req) {
+        $("#loadAllRaavareBatchList").empty();
+        $.each(data, function (i, raavareBatch) {
+            $("#loadAllRaavareBatchList").append(genTableHTMLForRaavareBatch(raavareBatch));
+        });
+    });
+}
 
-function loadRaavareBatch() {
+function genTableHTMLForRaavareBatch(raavareBatch) {
+    return '<tr rbId="' + raavareBatch.rbId + '">' +
+        '<td>'+ raavareBatch.rbId + ' ' + raavareBatch.raavareId +'</td>' +
+        '<td>' + raavareBatch.maengde + '</td>' +
+        '<td><button class="btn-alert" type="submit" onclick="deleteRaavareBatch(' + raavareBatch.rbId + ');">Slet</button></td>' +
+        '</tr>'
+}
 
+function deleteRaavareBatch(rbId) {
+    event.preventDefault();
+    $.ajax({
+        url: hostURL + rbId,
+        method: 'DELETE',
+        success: function (data) {
+            alert(' Råvare Batch med id: ' + rbId + ' er blevet slettet!')
+            loadRaavareBatchList();
+        }
+    });
 }
