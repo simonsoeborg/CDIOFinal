110b6d6b8b3abab0c20c846a7c8cb2d51e1a3357
Kristoffer <s180500@student.dtu.dk>
2020-06-10 14:21:08 +0200

    Små stavefejl rettet

--- src/main/webapp/js/RaavareAdmin.js
+++ src/main/webapp/js/RaavareAdmin.js
@@ -1,6 +1,10 @@
 <!-- Author: Kristoffer -->
 var hostURL = '/CDIOFinal_war_exploded/test/raavare/';
 
+$(document).ready(function () {
+    loadRaavareList();
+});
+
 function loadRaavareList() {
     console.log("Loading raavarer");
     $.get(hostURL, function (data, textStatus, req) {
@@ -13,10 +17,10 @@ function loadRaavareList() {
 
 function genTableHTMLForRaavare(raavare) {
     return '<tr><td>'+ raavare.raavareId + '</td>'  +
-           '<td>' + raavare.raavareNavn +'</td>' +
-           '<td>' + raavare.leverandoer + '</td>' +
-           '<td><button class="btn-alert" type="submit" onclick="deleteRaavare(' + raavare.raavareId + ');">Slet</button></td>' +
-            '</td>'
+        '<td>' + raavare.raavareNavn +'</td>' +
+        '<td>' + raavare.leveradoer + '</td>' +
+        '<td><button class="btn-alert" type="submit" onclick="deleteRaavare(' + raavare.raavareId + ');">Slet</button></td>' +
+        '</td>'
 }
 
 function deleteRaavare(raavareId) {
