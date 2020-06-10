cb744cbe355bf3758367c6b31db1ee3f36603ec2
SImon <fridolf.lindhard@gmail.com>
2020-06-10 09:38:20 +0200

    Slettet implements (ikke eksisterende interface)

--- src/main/java/Data/DBRecept.java
+++ src/main/java/Data/DBRecept.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
-public class DBRecept implements IDataHandlerDAO {
+public class DBRecept {
 
     private int receptId;
     private String receptNavn;
