a7e3bcf71762da044149dbc896b336b16924074f
SImon <fridolf.lindhard@gmail.com>
2020-06-12 11:57:04 +0200

    Deaktiverede brugere kan nu ses

--- src/main/webapp/js/brugerAdmin.js
+++ src/main/webapp/js/brugerAdmin.js
@@ -1,4 +1,6 @@
- var hostUserURL = '/CDIOFinal_war_exploded/test/users/';
+ // Load userSideMenu code functionality:
+ function includeUserSideMenu() {
+     $(".grid-contentSidebar").load("brugerAdminSidebar.html"); }
 
 function loadActiveUserList() {
     let getAllUsersURL = '/CDIOFinal_war_exploded/test/users/activated/';
@@ -10,6 +12,17 @@ function loadActiveUserList() {
         });
     });
 }
+
+ function loadDeactivatedUserList() {
+     let getAllDeactivatedUsersURL = '/CDIOFinal_war_exploded/test/users/deactivated/';
+     console.log("Loading deactivated Users");
+     $.get(getAllDeactivatedUsersURL, function (data, textStatus, req) {
+         $("#loadDeactivatedUserList").empty();
+         $.each(data, function (i, user) {
+             $("#loadDeactivatedUserList").append(genTableHTMLForUserList((user)));
+         });
+     });
+ }
 
 function genTableHTMLForUserList(user) {
     return  '<tr><td>' + user.id + '</td>' +
