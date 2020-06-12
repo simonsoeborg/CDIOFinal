4af2c9cc75f3478750b15933a5fd7c5c4dcf8cc6
SImon <fridolf.lindhard@gmail.com>
2020-06-12 14:16:02 +0200

    Tilføjelse af aktiver og deaktiver

--- src/main/webapp/js/brugerAdmin.js
+++ src/main/webapp/js/brugerAdmin.js
@@ -1,34 +1,74 @@
  // Load userSideMenu code functionality:
+ var hostUserURL = '/CDIOFinal_war_exploded/test/users/';
+
  function includeUserSideMenu() {
-     $(".grid-contentSidebar").load("brugerAdminSidebar.html"); }
+     $(".grid-contentSidebar").load("brugerAdminSidebar.html");}
+
 
 function loadActiveUserList() {
-    let getAllUsersURL = '/CDIOFinal_war_exploded/test/users/activated/';
+    let getAllActiveUsersURL = hostUserURL + 'activated';
     console.log("Loading Active Users");
-    $.get(getAllUsersURL, function (data, textStatus, req) {
+    $.get(getAllActiveUsersURL, function (data, textStatus, req) {
         $("#loadActiveUserList").empty();
         $.each(data, function (i, user) {
-            $("#loadActiveUserList").append(genTableHTMLForUserList((user)));
+            $("#loadActiveUserList").append(genTableHTMLForActiveUserList((user)));
         });
     });
 }
 
  function loadDeactivatedUserList() {
-     let getAllDeactivatedUsersURL = '/CDIOFinal_war_exploded/test/users/deactivated/';
+     let getAllDeactivatedUsersURL = hostUserURL+'deactivated';
      console.log("Loading deactivated Users");
      $.get(getAllDeactivatedUsersURL, function (data, textStatus, req) {
          $("#loadDeactivatedUserList").empty();
          $.each(data, function (i, user) {
-             $("#loadDeactivatedUserList").append(genTableHTMLForUserList((user)));
+             $("#loadDeactivatedUserList").append(genTableHTMLForDeactivatedUserList((user)));
          });
      });
  }
 
-function genTableHTMLForUserList(user) {
+
+ function deactivate (id) {
+     event.preventDefault();
+     $.ajax({
+         url: hostUserURL + 'deactivate' +'?id='+ id,
+         method: 'PUT',
+         success: function (data) {
+             alert(' Bruger med id: ' + id + ' er blevet deaktiveret !');
+             loadActiveUserList();
+         }
+     });
+ }
+
+ function activate (id) {
+     event.preventDefault();
+     $.ajax({
+         url: hostUserURL + 'activate' +'?id='+ id,
+         method: 'PUT',
+         success: function (data) {
+             alert(' Bruger med id: ' + id + ' er blevet aktiveret !');
+             loadDeactivatedUserList();
+         }
+     });
+ }
+
+
+function genTableHTMLForActiveUserList(user) {
     return  '<tr><td>' + user.id + '</td>' +
         '<td>' + user.firstname +'</td>' +
         '<td>' + user.lastname + '</td>' +
         '<td>' + user.initial + '</td>' +
         '<td>' + user.role + '</td>' +
-        '</tr>'
+        '<td><button class="btn-alert" type="submit" onclick="deactivate('+ user.id+' );">Deaktiver</button></td>' +
+        '</tr>';
 }
+
+ function genTableHTMLForDeactivatedUserList(user) {
+     return  '<tr><td>' + user.id + '</td>' +
+         '<td>' + user.firstname +'</td>' +
+         '<td>' + user.lastname + '</td>' +
+         '<td>' + user.initial + '</td>' +
+         '<td>' + user.role + '</td>' +
+         '<td><button class="btn-alert" type="submit" onclick="activate('+ user.id+' );">Aktiver</button></td>' +
+         '</tr>';
+ }
