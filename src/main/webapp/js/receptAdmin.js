7cb05d596ab19b272d04dd135bcb0d30263f19bd
Freja <frejaj9@gmail.com>
2020-06-14 21:33:09 +0200

    På det her tidspunkt kan jeg ikke finde ud af en skid

--- src/main/webapp/js/receptAdmin.js
+++ src/main/webapp/js/receptAdmin.js
@@ -1,39 +1,49 @@
+function genTableHTMLForRecepts(recept) {
+    return '<tr id="' + recept.receptid + ' '+ recept.receptname +'">' +
+    '<td>'+ recept.raavareid + '</td>' +
+    '<td>' + recept.nonnetto+ '</td>' +
+    '<td>' + recept.tolerance + '</td>' +
+    /*'<td><button class="btn-warning" type="submit" onclick="editRecept(' + recept.receptid + ');">Ret</button></td>' +
+    '<td><button class="btn-alert" type="submit" onclick="deleteUser(' + recept.receptid + ');">Slet</button></td>' +*/
+    '</tr>'
+}
 
-$(function (){
-    var $recepts = $('#recepts');
-    $.ajax({
-        type: 'GET',
-        url: '/CDIOFinal_war_exploded/test/recepts/',
-        success: function(recepts){
-            $.each(recepts, function(i, recept){
-                $recepts.append('<li>ReceptID: ' + recept.receptid + ', drink:' + recept.receptname + '</li>');
-            });
-        },
-        error: function () {
-            alert("error loading recepts")
-        }
-    })
-});
+function loadRecepts() {
+    let loadReceptUrl = '/CDIOFinal_war_exploded/test/recepts/';
+    console.log("Loading Recepts");
+    $.get(loadReceptUrl, function (data, textStatus, req) {
+        $("#loadAllRecepts").empty();
+        $.each(data, function (i, recept) {
+            $("#loadAllRecepts").append(genTableHTMLForRecepts(recept));
+        });
+    });
+}
+
 
 function dataCreateToJSON() {
     return JSON.stringify({
         "receptid": $('#receptid').val(),
         "receptname": $('#lastname').val(),
-    });
+        "nonnetto": $('#nonnetto').val(),
+        "tolerance": $('#tolerance').val()
+    })
 }
 
-function createUser(){
+function createUser() {
     var $receptid = $('#receptid');
     var $receptname = $('#receptname');
-    $("#add-recept").on('click', function(){
+    $("#add-recept").on('click', function () {
         $.ajax({
             type: 'POST',
+            url: '/CDIOFinal_war_exploded/test/recepts/',
             dataType: "json",
             data: dataCreateToJSON(),
-            success: function(newRecept){
-                $recepts.append('<li>ReceptID: ' + newRecept.receptid + ', drink:' + newRecept.receptname + '</li>');
+            success: function (newRecept) {
+                $recepts.append(genTableHTMLForRecepts(newRecept))
             }
-    });
+        })
+    })
 }
+
 
 
