1ffcb611bfb13e6c181442400ac592e7aa5039f9
Simon Søborg <s173255@student.dtu.dk>
2020-06-15 14:32:46 +0200

    Servlet, js og html

--- src/main/java/Controller/DBAfvejning.java
+++ src/main/java/Controller/DBAfvejning.java
@@ -4,10 +4,15 @@
 */
 package Controller;
 
+import Data.DTO.AfvejningReceptKomponent;
+import Data.DTO.Raavare;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DBAfvejning {
     DBConnector dbc = new DBConnector();
@@ -16,6 +21,7 @@ public class DBAfvejning {
 
     private String findBrugerQuery = "SELECT * FROM Brugere WHERE UserId = ?";
     private String findReceptQuery = "SELECT receptNavn FROM ProduktBatchAfvejning WHERE pbId = ?";
+    private String getReceptRaavareQuery = "SELECT raavareid, (SELECT raavarenavn FROM Raavare WHERE raavareid = ?), maengde, tolerance FROM ReceptKomponent WHERE pbId = ?";
 
     public String getLaborantName(int id) {
         SQLConn = dbc.createConnection();
@@ -62,4 +68,23 @@ public class DBAfvejning {
         return data;
     }
 
+    public List<AfvejningReceptKomponent> getAfvejningReceptKomponent(int id) {
+        ArrayList<AfvejningReceptKomponent> data = new ArrayList<>();
+        SQLConn = dbc.createConnection();
+        if(SQLConn != null) {
+            try {
+                sqlQuery = getReceptRaavareQuery;
+                PreparedStatement pstm = SQLConn.prepareStatement(sqlQuery);
+                pstm.setInt(1, id);
+                pstm.setInt(2, id);
+                ResultSet resultSet = pstm.executeQuery();
+                while (resultSet.next()) {
+                    data.add(new AfvejningReceptKomponent(resultSet.getInt("raavareid"), resultSet.getString("raavareNavn"), resultSet.getDouble("maengde"), resultSet.getDouble("tolerance")));
+                }
+            } catch (SQLException e) {
+                System.out.println(e);
+            }
+        }
+        return data;
+    }
 }
--- src/main/webapp/js/afvejning.js
+++ src/main/webapp/js/afvejning.js
@@ -2,25 +2,100 @@
 Author: Simon Søborg
 Github: simonsoeborg
 */
-var repeatI = 1;
+
+
+
+var IndexRepeat = 0;
 
 function repeatObject() {
+  if(IndexRepeat < 2) {
+    IndexRepeat++;
+    if(IndexRepeat >= 1) {
+      showRunAllBatchesButton();
+    }
+
+    if(IndexRepeat === 1) {
+      document.getElementById('repeatableObjectOne').style.display = "block";
+      document.getElementById('repeatableObjectOne').style.visibility = "visible";
+
+    }
+
+    if (IndexRepeat === 2) {
+      document.getElementById('repeatableObjectTwo').style.display = "block";
+      document.getElementById('repeatableObjectTwo').style.visibility = "visible";
+    }
+
+  } else {
+    alert("Der kan kun tilføjes max 3 afvejninger af gangen.");
+  }
+
   if(repeatI < 5) {
     if(repeatI >= 1) {
       showRunAllBatchesButton();
     }
-    var raavareOrig = document.getElementById('raavareOrig').id;
-    var maengdeOrig = document.getElementById('maengdeOrig').id;
-    var orig = document.getElementById('repeatableObject' + repeatI);
-    var repeat = orig.cloneNode(true);
-
-    repeat.id = "repeatableObject" + ++repeatI;
-
-    document.getElementById('raavareOrig').id = raavareOrig + repeatI;
-    document.getElementById('maengdeOrig').id = maengdeOrig + repeatI;
-
-    orig.parentNode.appendChild(repeat);
   }
+}
+
+function executeUpdateCommands(number) {
+  let afvejningHostURL = '/CDIOFinal_war_exploded/test/afvejning/';
+
+  if(number === 0) {
+    // Run only stuff from inputs in repeatableObjectZero
+    $.ajax({
+      type: 'POST',
+      contentType: 'application/json',
+      url: afvejningHostURL,
+      dataType: 'json',
+      data: afvejningToJSONZero(),
+      success: function (data, textStatus, req) {
+        var data0 = document.getElementById('taraBelastningZero').val();
+        var data1 = document.getElementById('raavareZero').val();
+        var data2 = document.getElementById('maengdeZero').val();
+        alert(data0 + ", " + data1 + ", " + data2 + " er blevet tilføjet til databasen!");
+      },
+      error: function () {
+
+      }
+    })
+  }
+
+  if(number === 1) {
+    // Run only stuff from inputs in repeatableObjectOne
+
+  }
+
+  if(number === 2) {
+    // Run only stuff from inputs in repeatableObjectTwo
+
+  }
+
+  if(number === 9) {
+    // Run all. For loop.
+  }
+}
+
+function afvejningToJSONZero() {
+  return JSON.stringify({
+    "tara": $('#taraBelastningZero').val(),
+    "rbid": $('#raavareZero').val(),
+    "rbId": $('#maengdeZero').val()
+  });
+}
+
+function afvejningToJSONOne() {
+  return JSON.stringify({
+    "tara": $('#taraBelastningOne').val(),
+    "rbid": $('#raavareOne').val(),
+    "rbId": $('#maengdeOne').val()
+  });
+}
+
+function afvejningToJSONTwo() {
+  return JSON.stringify({
+    "tara": $('#taraBelastningTwo').val(),
+    "rbid": $('#raavareTwo').val(),
+    "rbId": $('#maengdeTwo').val()
+  });
 }
 
 function showRunAllBatchesButton() {
@@ -39,6 +114,7 @@ function findLaborant() {
         alert("Error using " + labNr + "! \nTry again or use another laborant nr.");
       } else {
         document.getElementById('labNameResponse').innerHTML = "Laborant: " + result;
+        document.getElementById('wrapper').style.display = "block";
         document.getElementById('afvejningStep1').style.display = "none";
         document.getElementById('produktionStatus').style.visibility = "visible";
       }
@@ -62,10 +138,39 @@ function findProduktBatch() {
       } else {
         document.getElementById('receptNameResponse').innerHTML = "Recept: " + result;
         document.getElementById('produktBatchAndReceptName').style.display = "none";
+        getIngredients(produktbatchNr);
       }
     },
     error: function (jqXHR, textStatus, errorThrown) {
       alert("Unable to find receipt");
     }
   });
+}
+
+function getIngredients(pbId) {
+  let hostURLGetIngredients = "/CDIOFinal_war_exploded/test/afvejning/" + pbId;
+
+  $.ajax({
+    type: 'GET',
+    contentType: 'application/json',
+    url: hostURLGetIngredients,
+    dataType: 'json',
+    success: function (data, textStatus, req) {
+      $.each(data, function (index, value) {
+        $("#getIngredients").append(genTableHTMLForAfvejningTable(value))
+      })
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log('Der skete en fejl ved forsøg på indhentning af data' + textStatus);
+    }
+  })
+}
+
+function genTableHTMLForAfvejningTable(value) {
+  let data = '<tr>' +
+              '<td>'+ value.raavareId +'</td>' +
+              '<td>' + value.raavarenavn +'</td>' +
+              '<td>'+ value.maengde + '</td>' +
+              '<td>'+ value.tolerance + '</td>';
+  return data;
 }
