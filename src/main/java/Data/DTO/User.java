2c76d93c6e52a28a8fe0b179e1004587075f4e10
SImon <fridolf.lindhard@gmail.com>
2020-06-16 10:15:29 +0200

    Ændring i kommentering

--- src/main/java/Data/DTO/User.java
+++ src/main/java/Data/DTO/User.java
@@ -25,7 +25,7 @@ public class User {
         this.status=status;
     }
 
-    // *Todo Lav en løsning sålede at initials selv bliver dannet. (Ved ikke hvor jeg skal implementer).
+    // Is implemented somewhere else as of now. .
     public void setInitial (String firstname, String lastname) {
         Character first = Character.toUpperCase(firstname.charAt(0));
         Character second = Character.toUpperCase(lastname.charAt(0));
