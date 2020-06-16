13139a2a3c7756e7cfb583ecff266154eb1628cc
KEHansen <s195488@student.dtu.dk>
2020-06-16 13:10:33 +0200

    Fix af råvarenavn på råvarebatch.

--- src/main/java/Controller/DBRaavareBatch.java
+++ src/main/java/Controller/DBRaavareBatch.java
@@ -34,7 +34,7 @@ public class DBRaavareBatch {
                 PreparedStatement pstm = SQLConn.prepareStatement(sqlQuery);
                 ResultSet resultSet = pstm.executeQuery();
                 while (resultSet.next()) {
-                    data.add(new RaavareBatch(resultSet.getInt("rbId"), resultSet.getInt("raavareId"), resultSet.getDouble("maengde"), resultSet.getString("leverandoer"), resultSet.getString("raavareNavn")));
+                    data.add(new RaavareBatch(resultSet.getInt("rbId"), resultSet.getInt("raavareId"), resultSet.getDouble("maengde"), resultSet.getString("leverandoer"), resultSet.getString("raavarenavn")));
                 }
                 SQLConn.close();
             } catch (SQLException e) {
