57572dc0cb6e3045a9a307eede26a1f4325ff405
Kristoffer <s180500@student.dtu.dk>
2020-06-19 09:56:20 +0200

    sidder med if statement af login

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -11,10 +11,41 @@ function resetAll() {
     document.getElementById("sideMenuUL li").style.display = "none";
 }
 
+
 function admin() {
-    $(document).ready(function(){
         displayContent('home.html');
-        $("#admin").hide();
-        $("#bob").show();
-    })
+
+}
+
+async function userLayout() {
+    var role = document.getElementsByClassName("content_role")[0].id;
+    console.log("check who is login in");
+
+// Show div html based on role
+    if (role === "loginAdministrator") {
+        $("div.administrator").show();
+        console.log("login as Administrator")
+
+    }
+
+    if (role === "loginFarmaceut"){
+        $("div.farmaceut").show();
+        console.log("login as Farmaceut")
+    }
+
+    if (role === "loginProduktionsleder"){
+        $("div.produktionsleder").show();
+        console.log("login as Produktionsleder")
+    }
+
+    if (role === "loginLaborant"){
+        $("div.laborant").show();
+        console.log("login as Laborant")
+    }
+
+    if (role === "loginDeveloper"){
+        $("div.developer").show();
+        console.log("login as Developer")
+    }
+    await displayContent('home.html')
 }
