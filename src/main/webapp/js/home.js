00467b5f3af4a43a7d0b2dbecb8bc7279a414335
SImon <fridolf.lindhard@gmail.com>
2020-06-22 13:37:29 +0200

    Indførelse af individuelle homepages (med indhold).

--- src/main/webapp/js/home.js
+++ src/main/webapp/js/home.js
@@ -1,3 +1,9 @@
+/*
+Author:Kristoffer Baumgarten & Simon Fridolf
+Github: Jackktis & IceMonk3y
+*/
+hostUserURL='/CDIOFinal_war_exploded/test/users/';
+
 function welcome() {
     let role = sessionStorage.getItem("role");
     document.getElementById('scriptName').innerHTML="Velkommen - Du er logget ind som " + role;
@@ -37,7 +43,43 @@ function access() {
         document.getElementById('raavarebatchAdminMenu').style.display = "block";
         document.getElementById('produktbatchAdminMenu').style.display = "block";
         document.getElementById('afvejningMenu').style.display = "block";
+        document.getElementById('usertable').style.display = "none";
+
     }
 }
 
+/*
+Author: Simon Fridolf
+Github: IceMonk3y
+*/
+/* Loading users with the relevant role into each home-page */
 
+function role() {
+    let role = sessionStorage.getItem("role");
+    if(role === 'Administrator' || role === 'Farmaceut'|| role==='Laborant'){
+        document.getElementById('role').innerHTML= "Systemets: " + role+"er";
+    }
+    else document.getElementById('role').innerHTML= "Systemets: " + role+"e";
+}
+
+async function loadUserList() {
+    let role = sessionStorage.getItem('role');
+    if (role !== 'Udvikler') {
+        let getAllUsersHomeURL = hostUserURL + "roles?role=" + role;
+        console.log(getAllUsersHomeURL);
+        console.log("Loading Users");
+        await $.get(getAllUsersHomeURL, function (data, textStatus, req) {
+            $("#loadUsersList").empty();
+            $.each(data, function (i, user) {
+                $("#loadUsersList").append(genTableHTMLForUsers((user)));
+            });
+        });
+        document.getElementsByClassName("loader")[0].className = "loader-disabled";
+    }
+}
+function genTableHTMLForUsers(user) {
+    return  '<tr><td>' + user.id + '</td>' +
+        '<td>' + user.firstname +'</td>' +
+        '<td>' + user.lastname + '</td>' +
+        '</tr>';
+}
